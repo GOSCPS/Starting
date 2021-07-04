6f566dfff51d7520ca8ea697d21a8f94bade72b5
chhdao <sudo.free@qq.com>
2021-07-04 18:01:05 +0800

    Update ConfigReader trait

--- src/engine/cfg/mod.rs
+++ src/engine/cfg/mod.rs
@@ -4,11 +4,14 @@
 //=============================================
 // 用来操作配置文件
 
+use alloc::string::String;
+
 /// 解析配置文件需要实现的接口
+/// 
 /// 所有函数以panic!("STR");报告错误
 pub trait ConfigParser {
     /// 解析配置文件
-    fn parse(&self, strs: &str);
+    fn parse(&mut self, strs: &str);
 
     /// 获取字符串值
     fn get_string(&self, key: &str) -> &str;
@@ -19,9 +22,9 @@ pub trait ConfigParser {
     /// 获取bool键值
     fn get_bool(&self, key: &str) -> bool;
 
-    /// 获取object
-    fn get_object(&self, key: &str) -> dyn ConfigParser;
-
     /// 是否包含某一键
     fn contain_key(&self, key: &str) -> bool;
+
+    /// 获取所有键值
+    fn keys(&self) -> &[String];
 }
